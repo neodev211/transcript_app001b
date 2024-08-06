9ba6ca534bb9846516e22d9efb1560ccc3682c97
Noel Angles <noel.iot.37@gmail.com>
2024-08-06 16:56:22 -0500

    test

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Flask Vercel Example - Hello World"), 200
+    return jsonify(message="Flask Vercel Example - Hello World5"), 200
 
 def get_video_id(url):
     # Extraer el ID del video de la URL de YouTube
